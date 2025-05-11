8eba7a2cc2209ab0f94adcca3d2f20b225ced85c
pascard20 <141923751+pascard20@users.noreply.github.com>
2025-05-11 11:27:43 +0200

    Add "Merge Sort" exercise

---
+++ mergeSort.js
@@ -0,0 +1,28 @@
+// Following the exercise from The Odin Project:
+// https://www.theodinproject.com/lessons/javascript-recursion
+
+const mergeSort = (numbers) => {
+  if (numbers.length < 2) return numbers;
+  const cutIndex = Math.floor(numbers.length / 2);
+
+  const leftArray = numbers.slice(0, cutIndex);
+  const rightArray = numbers.slice(cutIndex);
+
+  const sortedLeft = mergeSort(leftArray);
+  const sortedRight = mergeSort(rightArray);
+
+  return merge(sortedLeft, sortedRight);
+};
+
+const merge = (left, right) => {
+  const result = [];
+  let leftIndex = 0,
+    rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] <= right[rightIndex]) result.push(left[leftIndex++]);
+    else result.push(right[rightIndex++]);
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+};
